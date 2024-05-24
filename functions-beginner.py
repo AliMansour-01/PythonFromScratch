0a465125d90d7e997bd53a99a991e71ae658e21f
AliMansour-01 <alimansour2k3@gmail.com>
2024-05-24 01:03:53 -0400

    Update functions-beginner.py

--- functions-beginner.py
+++ functions-beginner.py
@@ -1,28 +1,92 @@
 #Hangman
 
 import random
-#Step 1
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 word = random.choice(word_list)
 
 empty_list = []
-game_over = False
 
-for i in range(len(word)):
+for e in range(len(word)):
     empty_list += "_"
+game_over = False
+lives = 6
 
-
-while not game_over:
-    guess = input("Guess a word: ").lower()
+while game_over == False:
+    guess = input("Guess a letter: ").lower()
     for position in range(len(word)):
         if guess == word[position]:
             empty_list[position] = guess
-    print(empty_list)
-
+    if guess not in word:
+        lives -= 1
     if '_' not in empty_list:
         game_over = True
-        print("You win!")
+        if game_over == True:
+            print("You Win!")
+    elif lives == 0:
+        print("You Lose!")
+        game_over = True
+    print(stages[lives])
+    print(f"{' '.join(empty_list)}")
+
 
 
 
