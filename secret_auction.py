8aa48c138fc246b7053f08b70e886be70b5fbd64
AliMansour-01 <alimansour2k3@gmail.com>
2024-05-29 23:18:31 -0400

    auction-program

---
+++ secret_auction.py
@@ -0,0 +1,15 @@
+#python Dictionaries, and Nesting
+
+import clear
+from secret_auction_art import logo
+
+print(logo)
+
+name = input("Enter Your name: ")
+bid = input("Enter your bid: $")
+
+def dictionary_func(n, b):
+    empty_dictionary = {}
+    
+
+
