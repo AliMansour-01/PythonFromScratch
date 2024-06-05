9e5b2bffb0252425627f185d487fd18642cd187f
AliMansour-01 <alimansour2k3@gmail.com>
2024-06-04 23:35:14 -0400

    calculator using functions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 #calculator using functions with outputs
 
-
+from calculator_art import logo
 
 #Calculator
 
@@ -20,25 +20,35 @@ def multiply(n1, n2):
 def divide(n1, n2):
    return n1 / n2
 
-operators = {
+operations = {
     "+": add,
     "-": sub,
     "*": multiply,
     "/": divide,
 }
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
 
-num1 = int(input("What's the first number?: "))
+    for keys in operations:
+        print(keys)
 
-for keys in operators:
-    print(keys)
-operations_symbol = input("Pick an operation from the above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operators[operations_symbol]
-answer = calculation_function(num1, num2)
+    should_continue = True
 
+    while should_continue:
+        operations_symbol = input("Pick an operation from the above: ")
+        num2 = float(input("What's the second number?: "))
+        calculation_function = operations[operations_symbol]
+        answer1 = calculation_function(num1, num2)
 
-print(f"{num1} {operations_symbol} {num2} = {answer}")
+        print(f"{num1} {operations_symbol} {num2} = {answer1}")
+        choice = input(f"Type 'y' to continue calculating with {answer1}, 'n' for new calculation, and 'q' to quit: ").lower()
+        if choice == 'y':
+            num1 = answer1
+        elif choice == 'n':
+            should_continue = False
+            calculator()
+        else:
+            should_continue = False
 
-
-
-
+calculator()
