4c603aac1c463c29035127e5da8e123a69618345
AliMansour-01 <alimansour2k3@gmail.com>
2024-08-20 16:51:14 -0400

    NATO-Alphabet-Project

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -1,19 +1,10 @@
-number = [1, 2, 3]
-new_numbers = [item + 1 for item in number]
-print(new_numbers)
+import pandas
 
-name = "Angela"
-new_list = [letter for letter in name]
-print(new_list)
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+data_dic = {row.letter: row.code for (index, row) in df.iterrows()}
 
-range_list = [num + num for num in range(1, 5)]
-print(range_list)
+word = input("Enter a word: ").upper()
 
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-
-short_names = [name for name in names if len(name) < 5]
-print(short_names)
-
-long_names = [name.upper() for name in names if len(name) > 5]
-print(long_names)
+letters_list = [data_dic[letter] for letter in word]
+print(letters_list)
