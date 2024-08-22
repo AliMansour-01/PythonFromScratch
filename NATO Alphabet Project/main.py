b39f1d21e854e323d4bb1346e6869c18cbbe59c7
AliMansour-01 <alimansour2k3@gmail.com>
2024-08-22 01:33:59 -0400

    Update main.py

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -4,7 +4,16 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 data_dic = {row.letter: row.code for (index, row) in df.iterrows()}
 
-word = input("Enter a word: ").upper()
 
-letters_list = [data_dic[letter] for letter in word]
-print(letters_list)
+def generate_phonetic():
+
+    word = input("Enter a word: ").upper()
+    try:
+        letters_list = [data_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(letters_list)
+
+generate_phonetic()
